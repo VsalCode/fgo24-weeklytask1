11f2f6f2dff78e8c39ae4caa3579ea9cbf3f4792
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-22 08:42:01 +0700

    Update javascript-2.js

--- javascript-2.js
+++ javascript-2.js
@@ -1,11 +1,13 @@
 const programDiskon = (totalBelanja) => {
 
   if(totalBelanja >= 1000000){
-    console.log(`Anda mendapat diskon 10%, total belanja anda menjadi Rp.${totalBelanja - (10 / 100)}`);
+    console.log(`Anda mendapat diskon 10%, total belanja anda menjadi Rp.${totalBelanja * (10 / 100)}`);
   } else if( totalBelanja >= 500000 && totalBelanja < 1000000 ){
-    console.log(`Anda mendapat diskon 10%, total belanja anda menjadi Rp.${totalBelanja - (5 / 100)}`);
-  } else {
-    console.log(`Anda tidak mendapat diskon, total belanja anda Rp.${totalBelanja - (5 / 100)}`);
+    console.log(`Anda mendapat diskon 10%, total belanja anda menjadi Rp.${totalBelanja * (5 / 100)}`);
+  } else if(totalBelanja <= 500000){
+    console.log(`Anda tidak mendapat diskon, total belanja anda Rp.${totalBelanja}`);
+  }else{
+    console.log(`Input harus berupa angka`);
   }
 }
 
